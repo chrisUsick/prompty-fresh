e6d1c90de5beebc0bb9fe0f3bc4798bf260f3fec
Chris Usick <christopher.usick@gmail.com>
2023-06-21 16:06:12 -0500

    initial commit

---
+++ routes/index.tsx
@@ -0,0 +1,12 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Promty</title>
+      </Head>
+      <p>Welcome to Prompty</p>
+    </>
+  );
+}
